c0630b5addcf0ef77b33565662dc29a3e9b1096a
Haewon Shin <hshin@pivotal.io>
2018-08-08 17:52:23 +0900

    Update spring_config.sh

--- lib/spring_config.sh
+++ lib/spring_config.sh
@@ -33,7 +33,5 @@ echo "starting spring_config"
 
 while read key value
 do
-	echo $key
-	echo $value
-#	export "$key"="$value"
+	export $key=$value
 done <<< "`python $DEPS_DIR/__BUILDPACK_INDEX__/spring_config.py`"
